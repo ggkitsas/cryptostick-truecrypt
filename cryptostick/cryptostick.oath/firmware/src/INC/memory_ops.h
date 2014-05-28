ae86f7d935bb10f55c94ab5c6f2a66748d1c3e16
ggkitsas <ggkitsas@yahoo.com>
2014-05-28 21:50:44 +0300

    removed firmware and gnuk

--- cryptostick/cryptostick.oath/firmware/src/INC/memory_ops.h
+++
@@ -1,3 +0,0 @@
-uint16_t getu16(uint8_t *array);
-uint32_t getu32(uint8_t *array);
-uint64_t getu64(uint8_t *array);
--- gnuk-1.1.2/src/fe25519.h
+++
@@ -1,4 +0,0 @@
-#define FE25519_WORDS 8
-typedef struct fe25519 {
-  uint32_t word[FE25519_WORDS]; /* Little endian */
-} fe25519;
